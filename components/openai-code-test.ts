2a15c65d1a43cf1b85dae5b4063ed7556c006ce4
Aman Shrestha <amanstha567@gmail.com>
2025-07-19 22:40:21 +1000

    Starting Chatbot Project

---
+++ components/openai-code-test.ts
@@ -0,0 +1,15 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: "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",
+});
+
+const completion = openai.chat.completions.create({
+  model: "gpt-4o-mini",
+  store: true,
+  messages: [
+    {"role": "user", "content": "write a haiku about ai"},
+  ],
+});
+
+completion.then((result) => console.log(result.choices[0].message));
